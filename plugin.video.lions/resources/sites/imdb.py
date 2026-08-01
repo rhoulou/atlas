ff2f8891fedd603de587acdae691e10d3ce04680
rhoulou <rhoulou@users.noreply.github.com>
2026-08-01 23:29:51 +0100

    v1.0.105 - rebuild IMDb + Top IMDb on IMDB Catalogs addon (browse lists + suggestion-API search), drop broken imdb.com scraping/genres

--- plugin.video.lions/resources/sites/imdb.py
+++ plugin.video.lions/resources/sites/imdb.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-# IMDb browse site (scrape www.imdb.com + legacy suggestion API for search)
+# IMDb browse + search site.
+# Browse lists come from the IMDB Catalogs Stremio addon (100 movies / 100 series, each with a real IMDb id);
+# search uses the IMDb legacy suggestion API (www.imdb.com scraping is blocked with HTTP 202).
 
 import re
-import json
 import urllib.parse
 
-from resources.lib.comaddon import addon, siteManager
+from resources.lib.comaddon import addon
 from resources.lib.gui.gui import cGui
 from resources.lib.handler.inputParameterHandler import cInputParameterHandler
 from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
@@ -15,69 +16,32 @@ SITE_IDENTIFIER = 'imdb'
 SITE_NAME = '[COLOR yellow]IMDb[/COLOR]'
 SITE_DESC = 'Internet Movie Database.'
 
-URL_MAIN = siteManager().getUrlMain(SITE_IDENTIFIER)
+CATALOG_BASE = 'https://1fe84bc728af-imdb-catalogs.baby-beamup.club'
+CATALOG_MOVIE = CATALOG_BASE + '/catalog/movie/imdb-movie-catalog.json'
+CATALOG_SERIES = CATALOG_BASE + '/catalog/series/imdb-series-catalog.json'
 
 SUGGEST_URL = 'https://v2.sg.media-imdb.com/suggestion/'
-
-MOVIE_TOP250 = (URL_MAIN + 'chart/top/', 'showMovies')
-MOVIE_POPULAR = (URL_MAIN + 'chart/moviemeter/', 'showMovies')
-SERIE_POPULAR = (URL_MAIN + 'chart/tvmeter/', 'showSeries')
 
 ADDON = addon()
 icons = ADDON.getSetting('defaultIcons')
 LOGO = 'special://home/addons/plugin.video.lions/resources/art/sites/imdb.png'
 
-GENRES = [
-    ('Action', 'action'),
-    ('Aventure', 'adventure'),
-    ('Animation', 'animation'),
-    ('Comedie', 'comedy'),
-    ('Crime', 'crime'),
-    ('Documentaire', 'documentary'),
-    ('Drame', 'drama'),
-    ('Famille', 'family'),
-    ('Fantastique', 'fantasy'),
-    ('Histoire', 'history'),
-    ('Horreur', 'horror'),
-    ('Musique', 'music'),
-    ('Mystere', 'mystery'),
-    ('Romance', 'romance'),
-    ('Science-Fiction', 'sci-fi'),
-    ('Thriller', 'thriller'),
-    ('Western', 'western')
-]
-
 
 def load():
     oGui = cGui()
-    addons = addon()
-    icons = addons.getSetting('defaultIcons')
+
     oOutputParameterHandler = cOutputParameterHandler()
     oOutputParameterHandler.addParameter('siteUrl', 'http://venom')
-    oGui.addDir(SITE_IDENTIFIER, 'showSearch', addons.VSlang(30330), icons + '/Search.png', oOutputParameterHandler)
+    oGui.addDir(SITE_IDENTIFIER, 'showSearch', ADDON.VSlang(30330), icons + '/Search.png', oOutputParameterHandler)
 
-    oOutputParameterHandler.addParameter('siteUrl', MOVIE_TOP250[0])
-    oGui.addDir(SITE_IDENTIFIER, MOVIE_TOP250[1], 'Top 250', LOGO, oOutputParameterHandler)
-
-    oOutputParameterHandler.addParameter('siteUrl', MOVIE_POPULAR[0])
-    oGui.addDir(SITE_IDENTIFIER, MOVIE_POPULAR[1], 'Films Populaires', LOGO, oOutputParameterHandler)
-
-    oOutputParameterHandler.addParameter('siteUrl', SERIE_POPULAR[0])
-    oGui.addDir(SITE_IDENTIFIER, SERIE_POPULAR[1], 'Series Populaires', LOGO, oOutputParameterHandler)
-
-    oOutputParameterHandler.addParameter('siteUrl', 'http://venom')
-    oGui.addDir(SITE_IDENTIFIER, 'showGenres', addons.VSlang(30428), icons + '/Genres.png', oOutputParameterHandler)
-
-    oGui.setEndOfDirectory()
-
-
-def showGenres():
-    oGui = cGui()
     oOutputParameterHandler = cOutputParameterHandler()
-    for sLabel, sGenre in GENRES:
-        sUrl = URL_MAIN + 'search/title/?title_type=feature&genres=' + sGenre + '&sort=moviemeter,asc'
-        oOutputParameterHandler.addParameter('siteUrl', sUrl)
-        oGui.addDir(SITE_IDENTIFIER, 'showMovies', sLabel, icons + '/Genres.png', oOutputParameterHandler)
+    oOutputParameterHandler.addParameter('siteUrl', CATALOG_MOVIE)
+    oGui.addDir(SITE_IDENTIFIER, 'showMovies', 'Films IMDb', icons + '/Movies.png', oOutputParameterHandler)
+
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', CATALOG_SERIES)
+    oGui.addDir(SITE_IDENTIFIER, 'showSeries', 'Series IMDb', icons + '/TVShows.png', oOutputParameterHandler)
+
     oGui.setEndOfDirectory()
 
 
@@ -125,68 +89,57 @@ def showSearchResults(sSearchText=''):
             sThumb = sImage.get('imageUrl', '')
         sQid = row.get('qid', '')
 
+        oOutputParameterHandler = __params(sTitle, sYear, sId)
         if sQid in ('tvSeries', 'tvMiniSeries'):
-            oGui.addTV('globalSearch', 'showSearch', sTitle, '', sThumb, sDesc, __params(sTitle, sYear, sId))
+            oGui.addTV('globalSearch', 'showSearch', sTitle, '', sThumb, sDesc, oOutputParameterHandler)
         else:
-            oGui.addMovie('globalSearch', 'showSearch', sTitle, '', sThumb, sDesc, __params(sTitle, sYear, sId))
+            oGui.addMovie('globalSearch', 'showSearch', sTitle, '', sThumb, sDesc, oOutputParameterHandler)
 
 
-def showMovies(sSearch=''):
+def showMovies():
+    __showCatalog('movie')
+
+
+def showSeries():
+    __showCatalog('series')
+
+
+def __showCatalog(sType):
     oGui = cGui()
     oInputParameterHandler = cInputParameterHandler()
-    if sSearch:
-        sUrl = sSearch
-    else:
-        sUrl = oInputParameterHandler.getValue('siteUrl')
+    sUrl = oInputParameterHandler.getValue('siteUrl')
+    if not sUrl:
+        sUrl = CATALOG_MOVIE if sType == 'movie' else CATALOG_SERIES
 
-    aRows = __getRows(sUrl)
-    if not aRows:
+    aMetas = []
+    try:
+        oRequestHandler = cRequestHandler(sUrl)
+        data = oRequestHandler.request(jsonDecode=True)
+        if data and isinstance(data, dict) and 'metas' in data:
+            aMetas = data['metas']
+    except Exception:
+        aMetas = []
+
+    bAdded = False
+    for meta in aMetas:
+        sId = meta.get('id') or meta.get('imdb_id') or ''
+        if not str(sId).startswith('tt'):
+            continue
+        sTitle = meta.get('name', '')
+        if not sTitle:
+            continue
+        sYear = meta.get('year', '')
+        sThumb = meta.get('poster', '')
+
+        oOutputParameterHandler = __params(sTitle, sYear, sId)
+        if sType == 'series':
+            oGui.addTV('globalSearch', 'showSearch', sTitle, '', sThumb, '', oOutputParameterHandler)
+        else:
+            oGui.addMovie('globalSearch', 'showSearch', sTitle, '', sThumb, '', oOutputParameterHandler)
+        bAdded = True
+
+    if not bAdded:
         oGui.addText(SITE_IDENTIFIER, '[COLOR red]Aucun résultat n\'a été trouvé.[/COLOR]')
-        oGui.setEndOfDirectory()
-        return
-
-    for aRow in aRows:
-        sTitle, sYear, sThumb, sFanart, sRating, sDesc, sId = aRow
-        sLabel = sTitle
-        if sRating:
-            sLabel = '%s [COLOR fuchsia]%s/10[/COLOR]' % (sTitle, sRating)
-        oGui.addMovie('globalSearch', 'showSearch', sLabel, '', sThumb, sDesc, __params(sTitle, sYear, sId))
-
-    sNextUrl = __getNextUrl(sUrl)
-    if sNextUrl:
-        oOutputParameterHandler = cOutputParameterHandler()
-        oOutputParameterHandler.addParameter('siteUrl', sNextUrl)
-        oGui.addNext(SITE_IDENTIFIER, 'showMovies', 'Suivant', oOutputParameterHandler)
-
-    oGui.setEndOfDirectory()
-
-
-def showSeries(sSearch=''):
-    oGui = cGui()
-    oInputParameterHandler = cInputParameterHandler()
-    if sSearch:
-        sUrl = sSearch
-    else:
-        sUrl = oInputParameterHandler.getValue('siteUrl')
-
-    aRows = __getRows(sUrl)
-    if not aRows:
-        oGui.addText(SITE_IDENTIFIER, '[COLOR red]Aucun résultat n\'a été trouvé.[/COLOR]')
-        oGui.setEndOfDirectory()
-        return
-
-    for aRow in aRows:
-        sTitle, sYear, sThumb, sFanart, sRating, sDesc, sId = aRow
-        sLabel = sTitle
-        if sRating:
-            sLabel = '%s [COLOR fuchsia]%s/10[/COLOR]' % (sTitle, sRating)
-        oGui.addTV('globalSearch', 'showSearch', sLabel, '', sThumb, sDesc, __params(sTitle, sYear, sId))
-
-    sNextUrl = __getNextUrl(sUrl)
-    if sNextUrl:
-        oOutputParameterHandler = cOutputParameterHandler()
-        oOutputParameterHandler.addParameter('siteUrl', sNextUrl)
-        oGui.addNext(SITE_IDENTIFIER, 'showSeries', 'Suivant', oOutputParameterHandler)
 
     oGui.setEndOfDirectory()
 
@@ -199,128 +152,3 @@ def __params(sTitle, sYear, sId):
     oOutputParameterHandler.addParameter('sYear', sYear)
     oOutputParameterHandler.addParameter('searchtext', sTitle)
     return oOutputParameterHandler
-
-
-def __getHtml(sUrl):
-    oRequestHandler = cRequestHandler(sUrl)
-    oRequestHandler.addHeaderEntry('Accept-Language', 'en-US,en;q=0.9')
-    oRequestHandler.addHeaderEntry('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
-    oRequestHandler.addHeaderEntry('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8')
-    return oRequestHandler.request()
-
-
-def __getRows(sUrl):
-    sHtmlContent = __getHtml(sUrl)
-    if not sHtmlContent:
-        return None
-
-    aRows = __jsonRows(sHtmlContent)
-    if aRows is None:
-        aRows = __regexRows(sHtmlContent)
-    return aRows
-
-
-def __jsonRows(sHtmlContent):
-    oMatch = re.search(r'<script id="__NEXT_DATA__" type="application/json">(.*?)</script>', sHtmlContent, re.S)
-    if not oMatch:
-        return None
-    try:
-        data = json.loads(oMatch.group(1))
-    except Exception:
-        return None
-
-    aTitles = []
-    __collectTitleRows(data, aTitles)
-    if not aTitles:
-        return None
-
-    aRows = []
-    for row in aTitles:
-        sId = row.get('id', '')
-        sTitle = ''
-        if isinstance(row.get('titleText'), dict):
-            sTitle = row['titleText'].get('text', '')
-        if not sTitle:
-            sTitle = row.get('title', '') or row.get('l', '')
-
-        sYear = ''
-        if isinstance(row.get('releaseYear'), dict):
-            sYear = row['releaseYear'].get('year', '')
-        if not sYear:
-            sYear = row.get('y', '')
-
-        sThumb = ''
-        sFanart = ''
-        sImage = row.get('primaryImage') or row.get('image') or row.get('i')
-        if isinstance(sImage, dict):
-            sThumb = sImage.get('url', '') or sImage.get('imageUrl', '')
-            sFanart = sThumb
-
-        sRating = ''
-        if isinstance(row.get('ratingsSummary'), dict):
-            sRating = row['ratingsSummary'].get('aggregateRating', '')
-        if not sRating:
-            sRating = row.get('rating', '')
-
-        sDesc = ''
-        if isinstance(row.get('plot'), dict):
-            sPlot = row['plot'].get('plotText', '')
-            if isinstance(sPlot, dict):
-                sDesc = sPlot.get('plainText', '')
-        if not sDesc:
-            sDesc = row.get('plot', '') if isinstance(row.get('plot', ''), str) else ''
-
-        aRows.append((sTitle, sYear, sThumb, sFanart, str(sRating), sDesc, sId))
-    return aRows
-
-
-def __collectTitleRows(obj, aOut):
-    if isinstance(obj, dict):
-        sId = obj.get('id')
-        if isinstance(sId, str) and re.match(r'^tt\d+$', sId):
-            if any(k in obj for k in ('titleText', 'title', 'l')):
-                aOut.append(obj)
-        for v in obj.values():
-            __collectTitleRows(v, aOut)
-    elif isinstance(obj, list):
-        for item in obj:
-            __collectTitleRows(item, aOut)
-
-
-def __regexRows(sHtmlContent):
-    aRows = []
-    sPattern = 'href="/title/(tt[0-9]+)/[^"]*"[^>]*>.*?alt="([^"]+)".*?src="([^"]+)"'
-    oParser = __getParser()
-    aResult = oParser.parse(sHtmlContent, sPattern)
-    if not aResult[0]:
-        return None
-
-    for aEntry in aResult[1]:
-        sId, sTitle, sThumb = aEntry[0], aEntry[1], aEntry[2]
-        sYear = ''
-        oYear = re.search(r'\(([0-9]{4})\)', sHtmlContent[max(0, sHtmlContent.find(sTitle) - 200):sHtmlContent.find(sTitle) + 300])
-        if oYear:
-            sYear = oYear.group(1)
-        sRating = ''
-        oRating = re.search(r'ipc-rating-star--rating[^>]*>\s*([0-9]+\.[0-9]+)', sHtmlContent[max(0, sHtmlContent.find(sTitle) - 300):sHtmlContent.find(sTitle) + 300])
-        if oRating:
-            sRating = oRating.group(1)
-        aRows.append((sTitle, sYear, sThumb, sThumb, sRating, '', sId))
-    return aRows
-
-
-def __getParser():
-    from resources.lib.parser import cParser
-    return cParser()
-
-
-def __getNextUrl(sUrl):
-    if 'search/title' not in sUrl:
-        return None
-    oMatch = re.search(r'[?&]start=([0-9]+)', sUrl)
-    if oMatch:
-        iStart = int(oMatch.group(1)) + 50
-        return re.sub(r'([?&])start=[0-9]+', r'\1start=%d' % iStart, sUrl)
-    if '?' in sUrl:
-        return sUrl + '&start=51'
-    return sUrl + '?start=51'
--- plugin.video.lions/resources/sites/topimdb.py
+++ plugin.video.lions/resources/sites/topimdb.py
@@ -1,195 +1,53 @@
 # -*- coding: utf-8 -*-
-# vStream https://github.com/Kodi-vStream/venom-xbmc-addons
+# Top IMDb (series) - lists the 100-series IMDb catalog from the IMDB Catalogs Stremio addon.
 
-import re
-import unicodedata
-
-from resources.lib.comaddon import progress, addon
 from resources.lib.gui.gui import cGui
-from resources.lib.handler.inputParameterHandler import cInputParameterHandler
 from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
 from resources.lib.handler.requestHandler import cRequestHandler
-from resources.lib.parser import cParser
-from resources.lib.util import Quote
-
-try:
-    xrange
-except NameError:
-    xrange = range
-
-ADDON = addon()
-icons = ADDON.getSetting('defaultIcons')
-LOGO = 'special://home/addons/plugin.video.lions/resources/sites/logos/topimdb.png'
 
 SITE_IDENTIFIER = 'topimdb'
-SITE_NAME = '[COLOR orange]Top 1000 IMDb[/COLOR]'
-SITE_DESC = 'Base de donnees videos.'
+SITE_NAME = 'Top IMDb Series'
+SITE_DESC = 'Top series catalogue IMDb.'
 
-URL_MAIN = 'https://www.imdb.com/'
-POSTER_URL = 'https://ia.media-imdb.com/images/m/'
-FANART_URL = 'https://ia.media-.imdb.com/images/m/'
+CATALOG_BASE = 'https://1fe84bc728af-imdb-catalogs.baby-beamup.club'
+CATALOG_SERIES = CATALOG_BASE + '/catalog/series/imdb-series-catalog.json'
 
-MOVIE_WORLD = (URL_MAIN + 'search/title?groups=top_1000&sort=user_rating,desc&start=1', 'showMovies')
-MOVIE_TOP250 = (URL_MAIN + 'search/title?count=100&groups=top_250', 'showMovies')
-MOVIE_ANNEES = (True, 'showMovieYears')
-
-
-def unescape(text):
-    try:  # python 2
-        import htmlentitydefs
-    except ImportError:  # Python 3
-        import html.entities as htmlentitydefs
-
-    def fixup(m):
-        text = m.group(0)
-        if text[:2] == "&#":
-            # character reference
-            try:
-                if text[:3] == "&#x":
-                    return unichr(int(text[3:-1], 16))
-                else:
-                    return unichr(int(text[2:-1]))
-            except ValueError:
-                pass
-        else:
-            # named entity
-            try:
-                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
-            except KeyError:
-                pass
-        return text  # leave as is
-    return re.sub("&#?\w+;", fixup, text)
+LOGO = 'special://home/addons/plugin.video.lions/resources/art/sites/topimdb.png'
 
 
 def load():
     oGui = cGui()
 
-    oOutputParameterHandler = cOutputParameterHandler()
-    oOutputParameterHandler.addParameter('siteUrl', MOVIE_WORLD[0])
-    oGui.addDir(SITE_IDENTIFIER, MOVIE_WORLD[1], 'Top Films Mondial',LOGO, oOutputParameterHandler)
-
-    oOutputParameterHandler.addParameter('siteUrl', MOVIE_TOP250[0])
-    oGui.addDir(SITE_IDENTIFIER, MOVIE_TOP250[1], 'Top 250',LOGO, oOutputParameterHandler)
-
-    oOutputParameterHandler.addParameter('siteUrl', MOVIE_ANNEES[0])
-    oGui.addDir(SITE_IDENTIFIER, MOVIE_ANNEES[1], 'Top (Par Années)', icons + '/Calendar.png', oOutputParameterHandler)
-
-    oGui.setEndOfDirectory()
-
-
-def showMovieYears():
-    oGui = cGui()
-
-    import datetime
-    now = datetime.datetime.now()
-
-    oOutputParameterHandler = cOutputParameterHandler()
-    for i in reversed(xrange(1903, int(now.year))):
-        oOutputParameterHandler.addParameter('siteUrl', URL_MAIN + 'search/title?year=' + str(i) + ',' + str(i) + '&title_type=feature&explore=languages')
-        oGui.addDir(SITE_IDENTIFIER, 'showMovies', str(i), icons + '/Calendar.png', oOutputParameterHandler)
-
-    oGui.setEndOfDirectory()
-
-
-def showMovies(sSearch=''):
-    oGui = cGui()
-    oParser = cParser()
-    # bGlobal_Search = False
-
-    oInputParameterHandler = cInputParameterHandler()
-    if sSearch:
-        sUrl = sSearch
-    else:
-        sUrl = oInputParameterHandler.getValue('siteUrl')
-    # if URL_SEARCH[0] in sSearch:
-        # bGlobal_Search = True
-
-    oRequestHandler = cRequestHandler(sUrl)
-    oRequestHandler.addHeaderEntry('Accept-Language', 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3')
-    sHtmlContent = oRequestHandler.request()
-
-    sPattern = 'img alt="([^"]+).+?loadlate="([^"]+).+?primary">([^<]+).+?unbold">([^<]+).+?(?:|rated this(.+?)\s.+?)muted">([^<]+)'
-    aResult = oParser.parse(sHtmlContent, sPattern)
-
-    if aResult[0]:
-        total = len(aResult[1])
-        progress_ = progress().VScreate(SITE_NAME)
-        oOutputParameterHandler = cOutputParameterHandler()
-        for aEntry in aResult[1]:
-            progress_.VSupdate(progress_, total)
-            if progress_.iscanceled():
-                break
-
-            # sTitle = unicode(aEntry[0], 'utf-8')  # converti en unicode
-            # sTitle = unicodedata.normalize('NFD', sTitle).encode('ascii', 'ignore')  # vire accent
-            # sTitle = unescape(str(aEntry[1]))
-            # sTitle = sTitle.encode( "utf-8")
-
-            sTitle = ('%s %s [COLOR fuchsia]%s[/COLOR]') % (aEntry[2], aEntry[0], aEntry[4])
-            sThumb = aEntry[1].replace('UX67', 'UX328').replace('UY98', 'UY492').replace('67', '0').replace('98', '0')
-            sYear = re.search('([0-9]{4})', aEntry[3]).group(1)
-            sDesc = aEntry[5]
-
-            oOutputParameterHandler.addParameter('siteUrl', 'none')
-            oOutputParameterHandler.addParameter('sMovieTitle', str(aEntry[0]))
-            oOutputParameterHandler.addParameter('sYear', sYear)
-            oOutputParameterHandler.addParameter('searchtext', showTitle(str(aEntry[0]), str('none')))
-            oGui.addMovie('globalSearch', 'showSearch', sTitle, '', sThumb, sDesc, oOutputParameterHandler)
-
-        progress_.VSclose(progress_)
-
-        sNextPage = __checkForNextPage(sHtmlContent)
-        if sNextPage:
-            oOutputParameterHandler = cOutputParameterHandler()
-            oOutputParameterHandler.addParameter('siteUrl', sNextPage)
-            oGui.addNext(SITE_IDENTIFIER, 'showMovies', 'Suivant', oOutputParameterHandler)
-
-    if not sSearch:
-        oGui.setEndOfDirectory('500')
-
-
-def __checkForNextPage(sHtmlContent):
-    oParser = cParser()
-    sPattern = 'href="([^"]+?)"class="lister-page-next'
-    aResult = oParser.parse(sHtmlContent, sPattern)
-
-    if aResult[0]:
-        sUrl = ('%s/%s') % (URL_MAIN, aResult[1][0])
-        return sUrl
-
-    return False
-
-
-def showTitle(sMovieTitle, sUrl):
-
-    sExtraTitle = ''
-    # si c'est une série
-    if sUrl != 'none':
-        sExtraTitle = sUrl.split('|')[1]
-        sMovieTitle = sUrl.split('|')[0]
-
+    aMetas = []
     try:
-        # ancien décodage
-        sMovieTitle = unicode(sMovieTitle, 'utf-8')  # converti en unicode pour aider aux conversions
-        sMovieTitle = unicodedata.normalize('NFD', sMovieTitle).encode('ascii', 'ignore').decode("unicode_escape")  # vire accent et '\'
-        sMovieTitle = sMovieTitle.encode("utf-8").lower()  # on repasse en utf-8
-    except:
-        sMovieTitle = sMovieTitle.lower()
+        oRequestHandler = cRequestHandler(CATALOG_SERIES)
+        data = oRequestHandler.request(jsonDecode=True)
+        if data and isinstance(data, dict) and 'metas' in data:
+            aMetas = data['metas']
+    except Exception:
+        aMetas = []
 
-    sMovieTitle = Quote(sMovieTitle)
-    sMovieTitle = re.sub('\(.+?\)', ' ', sMovieTitle)  # vire les tags entre parentheses
+    bAdded = False
+    for meta in aMetas:
+        sId = meta.get('id') or meta.get('imdb_id') or ''
+        if not str(sId).startswith('tt'):
+            continue
+        sTitle = meta.get('name', '')
+        if not sTitle:
+            continue
+        sYear = meta.get('year', '')
+        sThumb = meta.get('poster', '')
 
-    # modif venom si le titre comporte un - il doit le chercher
-    sMovieTitle = re.sub(r'[^a-z -]', ' ', sMovieTitle)  # vire les caracteres a la con qui peuvent trainer
-    # sMovieTitle = re.sub('( |^)(le|la|les|du|au|a|l)( |$)', ' ', sMovieTitle)  # vire les articles
+        oOutputParameterHandler = cOutputParameterHandler()
+        oOutputParameterHandler.addParameter('siteUrl', 'http://venom')
+        oOutputParameterHandler.addParameter('sMovieTitle', sTitle)
+        oOutputParameterHandler.addParameter('sImdbId', sId)
+        oOutputParameterHandler.addParameter('sYear', sYear)
+        oOutputParameterHandler.addParameter('searchtext', sTitle)
+        oGui.addTV('globalSearch', 'showSearch', sTitle, '', sThumb, '', oOutputParameterHandler)
+        bAdded = True
 
-    # vire les espaces multiples et on laisse les espaces sans modifs car certains codent avec %20 d'autres avec +
-    sMovieTitle = re.sub(' +', ' ', sMovieTitle)
-    # modif ici
-    if sExtraTitle:
-        sMovieTitle = sMovieTitle.replace('%C3%A9', 'e').replace('%C3%A0', 'a')
-        sMovieTitle = sMovieTitle + sExtraTitle
-    else:
-        sMovieTitle = sMovieTitle
+    if not bAdded:
+        oGui.addText(SITE_IDENTIFIER, '[COLOR red]Aucun résultat n\'a été trouvé.[/COLOR]')
 
-    return sMovieTitle
+    oGui.setEndOfDirectory()
